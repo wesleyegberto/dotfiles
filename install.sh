aafa5179d53e9c7a3a3cdc64215dcdce8a08b94e
Wesley Egberto <wesleyegberto@gmail.com>
2020-01-09 00:37:47 -0300

    backup of existing .vimrc and init.vim

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@
 echo 'Installing .vimrc'
 
 echo 'Copying to ~/.vimrc'
+cp ~/.vimrc ~/.vimrc_backup
 ln -s `pwd`/.vimrc ~/.vimrc
 
 echo 'Copying to ~/.config/nvim/init.vimrc'
+cp ~/.config/nvim/init.vim ~/.config/nvim/init.vim_backup
 ln -s `pwd`/.vimrc ~/.config/nvim/init.vim
