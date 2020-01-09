fe3c1c4c6808f6bf48efa7dd962506e8db07ed1e
Wesley Egberto <wesleyegberto@gmail.com>
2020-01-09 00:13:23 -0300

    improvded install to use symbolic link

--- install.sh
+++ install.sh
@@ -1,1 +1,9 @@
-ln -s .vimrc ~/.vimrc_dotfiles
+# Script to install my files
+
+echo 'Installing .vimrc'
+
+echo 'Copying to ~/.vimrc'
+ln -s `pwd`/.vimrc ~/.vimrc
+
+echo 'Copying to ~/.config/nvim/init.vimrc'
+ln -s `pwd`/.vimrc ~/.config/nvim/init.vimrc
