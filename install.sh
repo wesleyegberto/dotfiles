3ddd46d06afbae9940a3c911cf27d5a99869dca0
Wesley Egberto <wesleyegberto@gmail.com>
2020-01-09 00:15:52 -0300

    fix path to nvim config

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ echo 'Copying to ~/.vimrc'
 ln -s `pwd`/.vimrc ~/.vimrc
 
 echo 'Copying to ~/.config/nvim/init.vimrc'
-ln -s `pwd`/.vimrc ~/.config/nvim/init.vimrc
+ln -s `pwd`/.vimrc ~/.config/nvim/init.vim
