cca1aaf5c43c5d3aca2fb31b20233ee2bd64146c
Wesley Egberto <wesleyegberto@gmail.com>
2021-09-01 23:41:31 -0300

    update mappgins

--- nvim/lua/plug-config/nvim-tree.lua
+++ nvim/lua/plug-config/nvim-tree.lua
@@ -64,7 +64,9 @@ g.nvim_tree_icons = {
 g.nvim_tree_bindings = {
   { key = { "<CR>", "o", "<2-LeftMouse>" }, cb = tree_cb "edit" },
   { key = { "<2-RightMouse>", "<C-]>" }, cb = tree_cb "cd" },
+  { key = "v", cb = tree_cb "vsplit" },
   { key = "<C-v>", cb = tree_cb "vsplit" },
+  { key = "s", cb = tree_cb "split" },
   { key = "<C-x>", cb = tree_cb "split" },
   { key = "<C-t>", cb = tree_cb "tabnew" },
   { key = "<", cb = tree_cb "prev_sibling" },
