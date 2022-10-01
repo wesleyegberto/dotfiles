17e89ddda725207422d5cfb6b6815501ab886a54
Wesley Egberto <wesleyegberto@gmail.com>
2022-09-30 23:15:09 -0300

    update config; added keyboard mapping

--- nvim/lua/plug-config/others-setup.lua
+++ nvim/lua/plug-config/others-setup.lua
@@ -8,20 +8,20 @@ require'nvim-autopairs'.setup()
 local ui = vim.api.nvim_list_uis()[1]
 
 require 'jabs'.setup {
-    position = 'center', -- center, corner
-   	width = 50,
-   	height = 10,
-   	border = 'rounded', -- none, single, double, rounded, solid, shadow, (or an array or chars)
+  position = 'center', -- center, corner
+  width = 150,
+  height = 10,
+  border = 'rounded', -- none, single, double, rounded, solid, shadow, (or an array or chars)
 
-    -- Options for preview window
-    preview_position = 'left', -- top, bottom, left, right
-    preview = {
-        width = 40,
-        height = 30,
-        border = 'double', -- none, single, double, rounded, solid, shadow, (or an array or chars)
-    },
+  -- Options for preview window
+  preview_position = 'left', -- top, bottom, left, right
+  preview = {
+    width = 40,
+    height = 30,
+    border = 'double', -- none, single, double, rounded, solid, shadow, (or an array or chars)
+  },
 
-   	-- the options below are ignored when position = 'center'
-    col = ui.width,  -- Window appears on the right
-    row = ui.height/2, -- Window appears in the vertical middle
+  -- the options below are ignored when position = 'center'
+  col = ui.width,  -- Window appears on the right
+  row = ui.height/2, -- Window appears in the vertical middle
 }
